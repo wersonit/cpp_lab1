2e882109eca8153d650e175558faa2f8cf928412
Георгий  Дримблёв <8201745@VC.MIET.RU>
2024-09-09 17:44:51 +0300

    comment number 1

---
+++ include/introduction.hpp
@@ -0,0 +1,43 @@
+/* Introduction class.
+ * @file
+ * @date 2021-09-04
+ * @author Anonymous
+ */
+
+#ifndef __INTRODUCTION_HPP__
+#define __INTRODUCTION_HPP__
+
+#include <string>
+
+// message number one
+
+//{
+class introduction
+{
+public:
+    std::string_view name() const
+    {
+        return ...
+    }
+
+//bolvanka-commit
+
+    std::string_view surename() const
+    {
+        return ...
+    }
+
+    std::string fullname() const
+    {
+        return ...
+    }
+
+//bolvanka-commit number two
+
+//bolvanka-commit number three
+
+private:
+};
+//}
+
+#endif // __INTRODUCTION_HPP__
